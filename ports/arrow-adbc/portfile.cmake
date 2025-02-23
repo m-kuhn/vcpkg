b97ac3a659d71057ac56f7353c489a4e901538e3
Matthias Kuhn <matthias@opengis.ch>
2025-02-23 15:55:42 +0100

    Unvendor dependencies

--- ports/arrow-adbc/portfile.cmake
+++ ports/arrow-adbc/portfile.cmake
@@ -7,7 +7,9 @@ vcpkg_from_github(
     PATCHES
         fix_static_build.patch
 	fix_windows_build.patch
+        unvendor.patch
 )
+file(REMOVE_RECURSE "${SOURCE_PATH}/c/vendor")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
